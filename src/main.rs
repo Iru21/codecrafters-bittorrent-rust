06aa508fca43e182369ef9f52f7b41ef22647760
Iru21 <33987046+Iru21@users.noreply.github.com>
2023-09-10 16:25:05 +0200

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,27 @@
-use serde_json;
 use std::env;
+use serde_bencode::{self, value::Value};
 
-// Available if you need it!
-// use serde_bencode
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    let first_char = encoded_value.chars().next().unwrap();
-
-    let rv: serde_json::Value;
-
-    if first_char.is_digit(10) {
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        rv = serde_json::Value::String(string.to_string());
-    } else if first_char == 'i' && encoded_value.ends_with('e') {
-        let number_string = &encoded_value[1..encoded_value.len() - 1];
-        let number = number_string.parse::<isize>().unwrap();
-        rv = serde_json::Value::Number(number.into());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+fn format(value: &Value) -> String {
+    return match value {
+        Value::Bytes(bytes) => match String::from_utf8(bytes.to_vec()) {
+            Ok(string) => string,
+            Err(_) => panic!("invalid UTF-8"),
+        },
+        Value::Int(i) => i.to_string(),
+        Value::List(list) => list.iter().map(format).collect::<Vec<String>>().join(","),
+        _ => panic!("invalid type"),
     }
-
-    return rv;
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let decoded_value = serde_bencode::from_str::<Value>(encoded_value).unwrap();
+        println!("{}", format(&decoded_value));
     } else {
         println!("unknown command: {}", args[1])
     }
