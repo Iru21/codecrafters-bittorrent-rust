8c7964916ae27e8f5496781b2093f2538ae8f96a
Iru21 <33987046+Iru21@users.noreply.github.com>
2023-09-10 23:27:25 +0200

    pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -74,23 +74,59 @@ impl Torrent {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct UrlEncoded {
-    value: String,
+
+fn url_encode(bytes: &[u8]) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(bytes);
+
+    let hashed = hasher.finalize().iter().map(|b| {
+        format!("%{:02x}", b)
+    }).collect::<Vec<String>>().join("");
+
+    hashed
 }
 
-impl UrlEncoded {
-    fn new(value: String) -> Self {
-        let mut hasher = Sha1::new();
-        hasher.update(value);
+#[derive(Debug, Serialize, Deserialize)]
+struct TrackerRequest {
+    info_hash: String,
+    peer_id: String,
+    port: u16,
+    uploaded: u64,
+    downloaded: u64,
+    left: u64,
+    compact: u8,
+}
 
-        let hashed = hasher.finalize().iter().map(|b| {
-            format!("%{:02x}", b)
-        }).collect::<Vec<String>>().join("");
-
-        UrlEncoded {
-            value: hashed
+impl TrackerRequest {
+    fn new(meta: TorrentInfo) -> Self {
+        TrackerRequest {
+            info_hash : url_encode(&serde_bencode::to_bytes(&meta).unwrap()),
+            peer_id: "00112233445566778899".to_string(),
+            port: 6881,
+            uploaded: 0,
+            downloaded: 0,
+            left: meta.length as u64,
+            compact: 1,
         }
+    }
+
+    fn fetch_peers(&self, tracker_url: String) -> TrackerResponse {
+        let client = reqwest::blocking::Client::new();
+
+        let url = format!("{}?info_hash={}", tracker_url, &self.info_hash);
+
+        let req = client.get(&url)
+            .query(&[("peer_id", self.peer_id.clone())])
+            .query(&[("port", self.port.to_string())])
+            .query(&[("uploaded", self.uploaded.to_string())])
+            .query(&[("downloaded", self.downloaded.to_string())])
+            .query(&[("left", self.left.to_string())])
+            .query(&[("compact", self.compact.to_string())])
+            .build().unwrap();
+
+        let res = client.execute(req).unwrap();
+
+        return serde_bencode::from_bytes(res.bytes().unwrap().as_ref()).unwrap()
     }
 }
 
@@ -102,7 +138,7 @@ struct TrackerResponse {
 }
 
 impl TrackerResponse {
-    fn peers(&self) -> Vec<String> {
+    fn format_peers(&self) -> Vec<String> {
         self.peers.chunks(6).map(|chunk| {
             let ip = chunk[0..4].iter().map(|b| {
                 format!("{}", b)
@@ -114,59 +150,6 @@ impl TrackerResponse {
         }).collect()
     }
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-struct TrackerRequest {
-    info_hash: UrlEncoded,
-    peer_id: String,
-    port: u16,
-    uploaded: u64,
-    downloaded: u64,
-    left: u64,
-    compact: u8,
-}
-
-impl TrackerRequest {
-    fn new(info_hash: String, left: u64) -> Self {
-        TrackerRequest {
-            info_hash : UrlEncoded::new(info_hash),
-            peer_id: "00112233445566778899".to_string(),
-            port: 6881,
-            uploaded: 0,
-            downloaded: 0,
-            left,
-            compact: 1,
-        }
-    }
-
-    fn peers(&self, tracker_url: String) -> TrackerResponse {
-        let client = reqwest::blocking::Client::new();
-
-        let url = format!("{}?info_hash={}", tracker_url, &self.info_hash.value);
-
-        let req = client.get(&url)
-            .query(&[("peer_id", self.peer_id.clone())])
-            .query(&[("port", self.port.to_string())])
-            .query(&[("uploaded", self.uploaded.to_string())])
-            .query(&[("downloaded", self.downloaded.to_string())])
-            .query(&[("left", self.left.to_string())])
-            .query(&[("compact", self.compact.to_string())])
-            .build().unwrap();
-
-        println!("Calling: {}", req.url());
-
-        let res = client.execute(req).unwrap();
-
-        let mut bytes= Vec::<u8>::new();
-        for byte in res.bytes().unwrap() {
-            bytes.push(byte);
-        }
-
-        return serde_bencode::from_bytes(bytes.as_slice()).unwrap()
-    }
-}
-
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -191,12 +174,12 @@ fn main() {
     } else if command == "peers" {
         let meta = Torrent::from_file(&args[2]);
 
-        let request = TrackerRequest::new(meta.info.hash(), meta.info.length as u64);
-        let response = request.peers(meta.announce);
+        let response = TrackerRequest::new(meta.info)
+            .fetch_peers(meta.announce);
 
         println!("Peers:");
 
-        for peer in response.peers() {
+        for peer in response.format_peers() {
             println!("{}", peer);
         }
     } else {
