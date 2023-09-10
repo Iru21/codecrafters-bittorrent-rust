0b3228e0e2ab31e44840f6cdfb2d3e2a0cc83fbb
Iru21 <33987046+Iru21@users.noreply.github.com>
2023-09-10 16:43:36 +0200

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use std::env;
 use serde_bencode::{self, value::Value};
 
+fn decode(encoded_value: &str) -> Value {
+    return serde_bencode::from_str::<Value>(encoded_value).unwrap();
+}
 
 fn format(value: &Value) -> String {
     return match value {
         Value::Bytes(bytes) => format!("{:?}", std::str::from_utf8(bytes).unwrap()),
         Value::Int(i) => i.to_string(),
         Value::List(list) => format!("[{}]", list.iter().map(format).collect::<Vec<String>>().join(",")),
-        _ => panic!("invalid type"),
+        Value::Dict(dict) => {
+            let mut result = String::from("{");
+            for (key, value) in dict {
+                result.push_str(&format!("\"{}\": {}", std::str::from_utf8(key).unwrap(), format(value)));
+
+                if key != dict.keys().last().unwrap() {
+                    result.push_str(", ");
+                }
+            }
+            result.push_str("}");
+            result
+        }
     }
 }
 
@@ -17,7 +31,7 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded_value = serde_bencode::from_str::<Value>(encoded_value).unwrap();
+        let decoded_value = decode(encoded_value);
         println!("{}", format(&decoded_value));
     } else {
         println!("unknown command: {}", args[1])
