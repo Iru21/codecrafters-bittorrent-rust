a892d4e747fa2072a07acee5ea172b479e31b16b
Iru21 <33987046+Iru21@users.noreply.github.com>
2023-09-11 15:40:43 +0200

    pass 10th stage

--- src/connection.rs
+++ src/connection.rs
@@ -78,11 +78,10 @@ impl Connection {
         println!("* Piece length: {}", meta.info.piece_length);
 
         const CHUNK_SIZE: usize = 16 * 1024;
-        let mut block_count = 0;
-        for byte_offset in (0..meta.info.piece_length).step_by(CHUNK_SIZE) {
-            println!("++ Requesting block {}", byte_offset / CHUNK_SIZE);
-            self.send_request(piece_index, byte_offset as u32, CHUNK_SIZE as u32);
-            block_count += 1;
+        let block_count = meta.info.piece_length / CHUNK_SIZE;
+        for i in 0..block_count {
+            println!("++ Requesting block {}", i);
+            self.send_request(piece_index, (i * CHUNK_SIZE) as u32, CHUNK_SIZE as u32);
         }
 
 
@@ -140,10 +139,7 @@ impl Connection {
         }
 
         let resp_size = u32::from_be_bytes(length_prefix) - 1;
-        println!("* Expecting {} bytes", resp_size);
-        if resp_size == 0 {
-            return vec![];
-        }
+        println!("* Expecting {} bytes of payload", resp_size);
         let mut payload = vec![0; resp_size as usize];
         self.stream.read_exact(&mut payload).expect("Failed to read payload");
 
